e15e4a2fee1d58e3a7f942f2b397f995a6adaf72
James <hacklander.dev@gmail.com>
2022-05-03 16:19:47 -0700

    Developed Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+  
 export default function Navbar() {
     return (
         <header>
-            <h1>James Hacklander</h1>
-
-            <nav>
-                <ul>
-                    <li>
-                        <Link to='/'>About me</Link>
-                    </li>
-                    <li>
-                        <Link to='/projects'>Projects</Link>
-                    </li>
-                    <li>
-                        <Link to='/contact'>Contact</Link>
-                    </li>
-                    <li>
-                        <a href='./James Hacklander resume.pdf' target='_blank' >Resume</a>
-                    </li>
-                </ul>
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <div class="container-fluid">
+                <h1 class="navbar-brand">James Hacklander</h1>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <Link to='/' class="nav-link">About Me</Link>
+                        <Link to='/projects' class="nav-link">Projects</Link>
+                        <Link to='/contact' class="nav-link">Contact</Link>
+                        <a href='./James Hacklander Resume.pdf' target='_blank' class="nav-link">Resume</a>
+                    </div>
+                </div>
+            </div>
             </nav>
         </header>
     )
 };
-
-
